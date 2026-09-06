c0980636dc2a95cd6625e91545bca7583d3fb663
mel0nze <richard@gmail.com>
2026-09-06 14:06:09 +0300

    Fixed not compiling by removing part of codebase

--- config.h
+++ config.h
@@ -123,10 +123,5 @@
 		{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 		{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 		{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-		{ ClkStatusText, 0, Button1, sigstatusbar, {.i = 1} },
-		{ ClkStatusText, 0, Button2, sigstatusbar, {.i = 2} },
-		{ ClkStatusText, 0, Button3, sigstatusbar, {.i = 3} },
-		{ ClkStatusText, 0, Button4, sigstatusbar, {.i = 4} },
-		{ ClkStatusText, 0, Button5, sigstatusbar, {.i = 5} },
 	};
 
